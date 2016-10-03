920da210db5b406e8e67d9db244321528ffae792
bstockus <stockusb@cityoflacrosse.org>
2016-10-03 15:44:50 -0500

    Minor Build Script Fix.

--- build.ps1
+++ build.ps1
@@ -58,7 +58,7 @@ exec { & dotnet restore }
 Invoke-MSBuild
 
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
-$revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
+$revision = "CI{0:D4}" -f [convert]::ToInt32($revision, 10)
 
 $packagesToPublish = @("Lax.Data", "Lax.Data.EntityFramework")
 
