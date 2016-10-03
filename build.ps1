5d3a530853e9d10b6a94a28f91375a7d4f33098e
bstockus <stockusb@cityoflacrosse.org>
2016-10-03 13:59:50 -0500

    Added AppVeyor Build Script and Config File.

---
+++ build.ps1
@@ -0,0 +1,17 @@
+﻿if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
+
+EnsurePsbuildInstalled
+
+exec { & dotnet restore }
+
+Invoke-MSBuild
+
+$revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
+$revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
+
+$packagesToPublish = @("Lax.Data", "Lax.Data.EntityFramework")
+
+foreach ($packageToPublish in $packagesToPublish) {
+	exec { & dotnet pack ".\src\" + $packagesToPublish -c Release -o .\artifacts --version-suffix=$revision }  
+}
+
