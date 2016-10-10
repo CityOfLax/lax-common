524cb4b65cc7a47fec04cfe4270eee4b3cff2c40
bstockus <stockusb@cityoflacrosse.org>
2016-10-10 13:36:43 -0500

    Added Service Collection Extensions to the Job Packages.

---
+++ src/Lax.Jobs.Runner.Synchronous/SynchronousJobRunnerServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Lax.Jobs.Runner.Synchronous {
+
+    public static class SynchronousJobRunnerServiceCollectionExtensions {
+
+        public static IServiceCollection AddSynchronousJobRunner(this IServiceCollection services) {
+
+            if (services == null) {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            services.TryAdd(ServiceDescriptor.Transient<IJobRunner, SynchronousJobRunner>());
+
+            return services;
+
+        }
+
+    }
+
+}
---
+++ src/Lax.Jobs/JobsServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Lax.Jobs {
+
+    public static class JobsServiceCollectionExtensions {
+
+        public static IServiceCollection AddJobs(this IServiceCollection services) {
+
+            if (services == null) {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            services.TryAdd(ServiceDescriptor.Transient<IJobExecutor, JobExecutor>());
+
+            return services;
+
+        }
+
+    }
+
+}
