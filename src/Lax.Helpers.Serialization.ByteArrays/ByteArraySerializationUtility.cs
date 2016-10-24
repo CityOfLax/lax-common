86d8d496e4925fbb659a467c4a50254d3a5810f0
bstockus <stockusb@cityoflacrosse.org>
2016-10-24 09:22:10 -0500

    Added Xml and Byte Array Serialization Helper Methods for UTF-8 and UTF-16.

--- src/Lax.Helpers.Serialization.ByteArrays/ByteArraySerializationUtility.cs
+++ src/Lax.Helpers.Serialization.ByteArrays/ByteArraySerializationUtility.cs
@@ -10,6 +10,18 @@ namespace Lax.Helpers.Serialization.ByteArrays {
         public static string SerializeByteArrayToDefaultString(byte[] value) =>
             Encoding.Default.GetString(value);
 
+        public static byte[] SerializeUTF8StringToByteArray(string value) =>
+            Encoding.UTF8.GetBytes(value);
+
+        public static string SerializeByteArrayToUTF8String(byte[] value) =>
+            Encoding.UTF8.GetString(value);
+
+        public static byte[] SerializeUTF16StringToByteArray(string value) =>
+            Encoding.Unicode.GetBytes(value);
+
+        public static string SerializeByteArrayToUTF16String(byte[] value) =>
+            Encoding.Unicode.GetString(value);
+
     }
 
 }
--- src/Lax.Helpers.Serialization.Xml/XmlSerializationUtility.cs
+++ src/Lax.Helpers.Serialization.Xml/XmlSerializationUtility.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Text;
 using System.Xml.Serialization;
 using Lax.Helpers.Serialization.ByteArrays;
 
@@ -15,6 +16,28 @@ namespace Lax.Helpers.Serialization.Xml {
             xmlSerializer.Serialize(stringWriter, obj);
             return stringWriter.ToString();
         }
+
+        private static byte[] SerializeToByteArrayWithEncoding<T>(T obj, Encoding encoding) {
+            var memoryStream = new MemoryStream();
+            var streamWriter = new StreamWriter(memoryStream, encoding);
+            var xmlSerializer = new XmlSerializer(typeof(T));
+
+            xmlSerializer.Serialize(streamWriter, obj);
+
+            return memoryStream.ToArray();
+        }
+
+        public static byte[] SerializeToUTF8ByteArray<T>(T obj) =>
+            SerializeToByteArrayWithEncoding(obj, Encoding.UTF8);
+
+        public static byte[] SerializeToUTF16ByteArray<T>(T obj) =>
+            SerializeToByteArrayWithEncoding(obj, Encoding.Unicode);
+
+        public static string SerializeToUTF8String<T>(T obj) =>
+            ByteArraySerializationUtility.SerializeByteArrayToUTF8String(SerializeToUTF8ByteArray(obj));
+
+        public static string SerializeToUTF16String<T>(T obj) =>
+            ByteArraySerializationUtility.SerializeByteArrayToUTF16String(SerializeToUTF16ByteArray(obj));
 
     }
 
