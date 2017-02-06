9f981a208ed3aef7d2dd1f56e787bc1060999e87
bstockus <stockusb@cityoflacrosse.org>
2017-02-06 10:45:30 -0600

    Added a Keyed EF Core Repository.

---
+++ src/Lax.Data.EntityFrameworkCore/EntityFrameworkCoreKeyedRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lax.Data.EntityFrameworkCore {
+
+    public class EntityFrameworkCoreKeyedRepository<TEntity, TPrimaryKey, TDbContext> : EntityFrameworkCoreRepository<TEntity, TDbContext>, IKeyedRepository<TEntity, TPrimaryKey> where TDbContext : DbContext where TEntity : class {
+
+        public EntityFrameworkCoreKeyedRepository(EntityFrameworkCoreRepositoryService<TDbContext> entityFrameworkCoreRepositoryService) : base(entityFrameworkCoreRepositoryService) {}
+
+        public async Task<TEntity> GetAsync(TPrimaryKey id) =>
+            await DbSet.FindAsync(id);
+
+    }
+
+}
