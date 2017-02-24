d6db4d88a008e5d258731d041629ebb24d67aff3
bstockus <stockusb@cityoflacrosse.org>
2017-02-24 10:30:17 -0600

    Added ReadOnly repository to Entity Framework Core code.

---
+++ src/Lax.Data.EntityFrameworkCore/EntityFrameworkCoreReadOnlyRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lax.Data.EntityFrameworkCore {
+
+    public abstract class EntityFrameworkCoreReadOnlyRepository<TEntity, TDbContext> : IReadOnlyRepository<TEntity> where TDbContext : DbContext where TEntity : class {
+
+        protected readonly EntityFrameworkCoreRepositoryService<TDbContext> EntityFrameworkCoreRepositoryService;
+
+        protected readonly DbSet<TEntity> DbSet;
+
+        protected EntityFrameworkCoreReadOnlyRepository(
+            EntityFrameworkCoreRepositoryService<TDbContext> entityFrameworkCoreRepositoryService) {
+
+            EntityFrameworkCoreRepositoryService = entityFrameworkCoreRepositoryService;
+            DbSet = EntityFrameworkCoreRepositoryService.DbSet<TEntity>();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync() => await DbSet.ToListAsync();
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> predicate)
+            => await DbSet.Where(predicate).ToListAsync();
+
+        public async Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> predicate)
+            => await DbSet.FirstOrDefaultAsync(predicate);
+
+    }
+
+}
