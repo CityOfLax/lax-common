b73cd36ed3248459835346cdee973c7ff439ba27
bstockus <stockusb@cityoflacrosse.org>
2017-03-10 17:10:02 -0600

    Minor Build Script Change

--- build.ps1
+++ build.ps1
@@ -51,7 +51,7 @@ function Exec
 
 if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
 
-& "$PSScriptRoot\dotnet-install.ps1" -Channel "production" -SharedRuntime -Version "latest" -Architecture x64
+& "$PSScriptRoot\dotnet-install.ps1" -SharedRuntime -Architecture x64
 
 dotnet -v
 
